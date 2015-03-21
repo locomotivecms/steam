78734e6729cdb2db1a56a96302824c89faff2550
did <didier.lafforgue@gmail.com>
2015-03-21 23:57:29 +0100

    do not load the locales if run within a Rails application

--- lib/locomotive/steam/initializers/i18n.rb
+++ lib/locomotive/steam/initializers/i18n.rb
@@ -1,3 +1,6 @@
-I18n.load_path = Dir[File.join(File.dirname(__FILE__), "/../../../../config/locales/*.yml")]
-I18n.enforce_available_locales = false
-I18n.backend.reload!
+unless defined?(Rails)
+  I18n.load_path << Dir[File.join(File.dirname(__FILE__), "/../../../../config/locales/*.yml")]
+
+  I18n.enforce_available_locales = false
+  I18n.backend.reload!
+end
