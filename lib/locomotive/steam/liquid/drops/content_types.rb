258ec9d2af9eb84ad27df93d270e933b775d5be4
did <didier.lafforgue@gmail.com>
2015-08-26 16:53:23 +0200

    new optimization: do not load the same content type twice (10% speed boost on a test site)

--- lib/locomotive/steam/liquid/drops/content_types.rb
+++ lib/locomotive/steam/liquid/drops/content_types.rb
@@ -5,9 +5,27 @@ module Locomotive
         class ContentTypes < ::Liquid::Drop
 
           def before_method(meth)
-            repository = @context.registers[:services].repositories.content_type
+            fetch_content_type(meth.to_s)
+          end
 
-            if content_type = repository.by_slug(meth.to_s)
+          private
+
+          def repository
+            @context.registers[:services].repositories.content_type
+          end
+
+          def fetch_content_type(slug)
+            @content_type_map ||= {}
+
+            if !@content_type_map.include?(slug)
+              @content_type_map[slug] = _fetch_content_type(slug)
+            end
+
+            @content_type_map[slug]
+          end
+
+          def _fetch_content_type(slug)
+            if content_type = repository.by_slug(slug)
               ContentEntryCollection.new(content_type)
             else
               nil
