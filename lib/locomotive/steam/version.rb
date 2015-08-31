2998affea868bf92bd37c9c895c48ac40a219215
did <didier.lafforgue@gmail.com>
2015-08-31 22:00:17 +0200

    bump version to 1.0.0-beta.3

--- lib/locomotive/steam/version.rb
+++ lib/locomotive/steam/version.rb
@@ -3,6 +3,6 @@
 # 1.0.0-alpha < 1.0.0-alpha.1 < 1.0.0-alpha.beta < 1.0.0-beta < 1.0.0-beta.2 < 1.0.0-beta.11 < 1.0.0-rc.1 < 1.0.0
 module Locomotive
   module Steam
-    VERSION = '1.0.0-beta.2'
+    VERSION = '1.0.0-beta.3'
   end
 end
