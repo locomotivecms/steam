6dc6c5d875351fe0ff7564055241a6d779f0f04e
did <didier.lafforgue@gmail.com>
2015-11-30 00:09:34 +0100

    redirect to the 404 page in the right locale if no entry found (fix issue locomotivecms/wagon#278)

--- lib/locomotive/steam/middlewares/templatized_page.rb
+++ lib/locomotive/steam/middlewares/templatized_page.rb
@@ -28,7 +28,8 @@ module Locomotive::Steam
           # log it
           log "Found content entry: #{entry._label}"
         else
-          redirect_to '/404', 302
+          url = services.url_builder.url_for(page_not_found, locale)
+          redirect_to url, 302
         end
       end
 
@@ -40,17 +41,21 @@ module Locomotive::Steam
         end
       end
 
-      def decorate(entry)
-        return nil if entry.nil?
-        Locomotive::Steam::Decorators::I18nDecorator.new(entry, locale, default_locale)
-      end
-
       def content_type_repository
         services.repositories.content_type
       end
 
       def content_entry_repository
         services.repositories.content_entry
+      end
+
+      def page_not_found
+        services.page_finder.find('404')
+      end
+
+      def decorate(entry)
+        return nil if entry.nil?
+        Locomotive::Steam::Decorators::I18nDecorator.new(entry, locale, default_locale)
       end
 
     end
