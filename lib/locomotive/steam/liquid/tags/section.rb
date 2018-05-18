894ac1e634ac2f2603e32e7fc788fc6017ff9812
Julien Girard <julien09girard@gmail.com>
2018-05-18 16:07:58 +0200

    update sections_content fetching

--- lib/locomotive/steam/liquid/tags/section.rb
+++ lib/locomotive/steam/liquid/tags/section.rb
@@ -21,7 +21,7 @@ module Locomotive
             # 3. because it's considered as a static section, go get the content from
             # the current site. If it doesn't exist, use the default attribute of
             # the section
-            section_content = context['site']&.sections_content&.fetch(@template_name, nil) #context["site"].sections[@template_name]
+            section_content = context['site']&.sections_content&.find{|a| a['id'] == @template_name}
 
             if section_content.blank?
               section_content = section.definition[:default] || {}
