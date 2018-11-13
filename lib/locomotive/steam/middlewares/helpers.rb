639a24fdff358909f99b24d7e49eddec64e92970
Sébastien BEAU <sebastien.beau@akretion.com>
2018-11-13 15:24:00 +0100

    [FIX] cookie should also be set in case of redirection

--- lib/locomotive/steam/middlewares/helpers.rb
+++ lib/locomotive/steam/middlewares/helpers.rb
@@ -89,9 +89,7 @@ module Locomotive::Steam
 
       def render_response(content, code = 200, type = nil)
         headers = { 'Content-Type' => type || 'text/html' }
-        request.env['steam.cookies'].each do |key, vals|
-          Rack::Utils.set_cookie_header!(headers, key, vals.symbolize_keys!)
-        end
+        inject_cookies(headers)
         @next_response = [code, headers, [content]]
       end
 
@@ -100,7 +98,16 @@ module Locomotive::Steam
 
         self.log "Redirected to #{_location}".blue
 
-        @next_response = [type, { 'Content-Type' => 'text/html', 'Location' => _location }, []]
+        headers = { 'Content-Type' => 'text/html', 'Location' => _location }
+        inject_cookies(headers)
+
+        @next_response = [type, headers, []]
+      end
+
+      def inject_cookies(headers)
+        request.env['steam.cookies'].each do |key, vals|
+          Rack::Utils.set_cookie_header!(headers, key, vals.symbolize_keys!)
+        end
       end
 
       def modify_path(path = nil, &block)
