477aef5861b40fd8e20aadbbfd3469824a89ca81
Didier Lafforgue <didier.lafforgue@gmail.com>
2018-11-21 21:37:40 +0100

    params supports JSON body (#109)

--- lib/locomotive/steam/middlewares/thread_safe.rb
+++ lib/locomotive/steam/middlewares/thread_safe.rb
@@ -66,7 +66,12 @@ module Locomotive::Steam::Middlewares
     end
 
     def params
-      @params ||= self.request.params.with_indifferent_access
+      @params ||= if request.content_type == 'application/json' && (request.post? || request.put?)
+        request.body.rewind
+        JSON.parse(request.body.read).with_indifferent_access
+      else
+        request.params.with_indifferent_access
+      end
     end
 
     def merge_with_params(values)
