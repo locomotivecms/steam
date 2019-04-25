f20e6861aa2e9cd3e10be9d150b396e2f1aa4148
Didier Lafforgue <didier.lafforgue@gmail.com>
2019-04-26 00:28:28 +0200

    bump versoin to 1.5.0.rc0

--- lib/locomotive/steam/version.rb
+++ lib/locomotive/steam/version.rb
@@ -3,6 +3,6 @@
 # 1.0.0.alpha < 1.0.0.alpha1 < 1.0.0.beta < 1.0.0.beta2 < 1.0.0.beta11 < 1.0.0.rc1 < 1.0.0
 module Locomotive
   module Steam
-    VERSION = '1.5.0.beta3'
+    VERSION = '1.5.0.rc0'
   end
 end
