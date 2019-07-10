e1f154e5de734de6d76eb46adecd606331d0200f
Weston Ganger <weston@westonganger.com>
2019-07-09 18:58:22 -0700

    Utilize global defaults for dropzone preset when `use_default` is defined

--- lib/locomotive/steam/adapters/filesystem/sanitizers/section.rb
+++ lib/locomotive/steam/adapters/filesystem/sanitizers/section.rb
@@ -41,6 +41,26 @@ module Locomotive::Steam
               definition['default'] = default
             end
 
+            # Utilize global defaults for dropzone preset when `use_default` is defined
+            if definition.key?('default') && definition.key?('dropzone_presets')
+              definition['dropzone_presets'].each_with_index do |preset_def, i|
+                if preset_def['use_default']
+                  # Fallback to use setting `default` key for Standalone/Global section settings and block settings
+                  definition['default']['settings'].each do |k,v|
+                    if !preset_def['settings'].key?(k)
+                      definition['dropzone_presets'][i]['settings'][setting['id']] = v
+                    end
+                  end
+
+                  preset_def['blocks'] ||= []
+                  definition['default']['blocks'] ||= []
+
+                  definition['dropzone_presets'][i]['blocks'] = preset_def['blocks'] + definition['default']['blocks']
+                end
+              end
+            end
+
+
             definition
           end
 
