fcaddb22370377fd469767bc610ac5046299e691
Didier Lafforgue <didier.lafforgue@gmail.com>
2023-02-25 22:02:02 +0100

    fix broken specs

--- spec/unit/middlewares/redirection_spec.rb
+++ spec/unit/middlewares/redirection_spec.rb
@@ -17,7 +17,7 @@ describe Locomotive::Steam::Middlewares::Redirection do
     env['steam.request']    = Rack::Request.new(env)
     env['locomotive.path']  = locomotive_path
     code, env = middleware.call(env)
-    [code, env['Location']]
+    [code, env['location']]
   end
 
   describe 'no redirection exception raised' do
